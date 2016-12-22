945ecdb654d323cbf059959d49755b01f453f91c
proofreadinguk <mbax9et3@Lefteris-MacBook-Pro.local>
2016-12-22 20:19:56 +0200

    before trying to use Plot.ly

--- stocks/app.R
+++ stocks/app.R
@@ -1,4 +1,6 @@
 library(quantmod)
+library(plotly)
+packageVersion('plotly')
 
 # TODO Separate Server and UI files 
 server <- function(input, output) {
@@ -10,88 +12,94 @@ server <- function(input, output) {
   observeEvent(input$y5, {  dateButtons$data <- 'last 5 years'  })
   observeEvent(input$y10,{  dateButtons$data <- 'last 10 years' })
   observeEvent(input$d3, {  dateButtons$data <- 'last 3 days'   }) 
-  observeEvent(input$w1, {  dateButtons$data <- 'last week'     })
+  observeEvent(input$w1, {  dateButtons$data <- 'last 1 week'   })
   observeEvent(input$m3, {  dateButtons$data <- 'last 3 months' })
   observeEvent(input$m6, {  dateButtons$data <- 'last 6 months' })
   
-  #minimum code because its the mostly run code
-  output$mainPlot <- renderPlot({
-    
+  stockData <- reactive({
     if(input$symb=="GOOG") outputSrc = "google"
     else outputSrc = "yahoo"
-  
-      #TODO add it into a Reaction
-    data <- getSymbols(input$symb, src = outputSrc, auto.assign = FALSE)
-    
-    chartSeries(data, theme = chartTheme("white"),  subset=dateButtons$data,
-                  type = "line", log.scale = FALSE, TA = c(addVo(),addBBands() ))
-
-      #addCCI(),addWPR() wiliams percentage , addROC() - rate of change, , addEVWMA(), addMACD()
-      # TODO Add more info
-      # TODO Show all the data it brings down so I understand what else I can use
-      
-      #getSymbols("USD/EUR",src="oanda") <- get exchange rates
-      #getFinancials Download and View Financial Statements
+    getSymbols(input$symb, src = outputSrc,  auto.assign = FALSE)
   })
   
-  output$text1 <- renderText({ {paste("Output Stock: ", input$symb)}})
-  output$text2 <- renderText({ {paste("Output Range: ", dateButtons$data)} })
+  currencyData <- reactive({ 
+    getFX(input$currency, from = "2012-01-01", to = Sys.Date(), auto.assign = FALSE)
+  })
+  
+  output$table <- renderDataTable(stockData())
+  
+  output$currencyPlot <-renderPlot({
+    chartSeries(currencyData(), theme = chartTheme("white"), subset=dateButtons$data, type = "line", log.scale = FALSE, TA =NULL)
+  })
+  
+  output$mainPlot <- renderPlot({
+      chartSeries(stockData(), theme = chartTheme("white"),  subset=dateButtons$data,
+                  type = "line", log.scale = FALSE, TA = c(addVo(),addBBands() ))
+      #addCCI(),addWPR() wiliams percentage , addROC() - rate of change, , addEVWMA(), addMACD()
+  })
+  
+  #getFinancials Download and View Financial Statements
+  
+  #output$text1 <- renderText({ {paste("Output Stock: ", input$symb)}})
+  #output$text2 <- renderText({ {paste("Output Range: ", dateButtons$data)} })
   
   #output$fin <- getFin(input$symb,auto.assign = TRUE)
   #output$stats <- renderPrint({ summary(rnorm(data) )  })
+  #output$plotlyPlot <- renderPlotly({
+  #  plot_ly(stockData(), x = ~mpg, y = ~wt)
+  #})
+  
+  #output$event <- renderPrint({
+  #  d <- event_data("plotly_hover")
+  #  if (is.null(d)) "Hover on a point!" else d
+  #})
 }
 
 ui <- fluidPage(
     titlePanel( h3("Stockmarket View of Data", align = "center") ),
-    
-      sidebarLayout(
-      position = "right",
+    sidebarLayout(
+    position = "right",
       sidebarPanel( 
+        h4("Select time span:"),
         fluidRow( 
           column(2,
-                 actionButton("d3", "3D"), 
-                 actionButton("y1", "1Y")
+            actionButton("d3", "3D"), 
+            actionButton("y1", "1Y")
           ),
-          
           column(2, offset = 1,
-                 actionButton("w1", "1W"), 
-                 actionButton("y3", "3Y")
+            actionButton("w1", "1W"), 
+            actionButton("y3", "3Y")
           ),
-          
+            
           column(2, offset = 1,
-                 actionButton("m3", "3M"), 
-                 actionButton("y5", "5Y")
+             actionButton("m3", "3M"), 
+             actionButton("y5", "5Y")
           ),
-          
+            
           column(2, offset = 1,
-                 actionButton("m6", "6M"),
-                 actionButton("y10", "10Y")
+             actionButton("m6", "6M"),
+             actionButton("y10", "10Y")
           )
         ), 
-    
+      
         br(),
         selectInput("symb", "Select Stock:",
-                    c("SPY" = "SPY", "FTSE"="^FTSE", "Google" = "GOOG","Amazon" = "AMZN", "Microsoft"="MSFT" ))
-        
-        #br(),
-        #dateRangeInput("dates", 
-        #               "Date range",
-        #               start = "2013-01-01", 
-        #               end = as.character(Sys.Date()))
-        
-        #br(),
-        #sliderInput("range", 
-        #            label = "Range of interest:",
-        #            min = 0, max = 100, value = c(0, 100))
+                      c("SPY" = "SPY", "FTSE"="^FTSE", "Google" = "GOOG","Amazon" = "AMZN", "Microsoft"="MSFT" )),
+        br(),
+        selectInput("currency", "Select Currency:",
+                    c("GBP/EUR", "EUR/GBP", "EUR/USD", "USD/EUR", "GBP/USD", "USD/GBP"))
       ),
-      mainPanel( plotOutput("mainPlot"), textOutput("text1") ,textOutput("text2"))
-      
-      #tabsetPanel(
-      #  tabPanel("Plot", plotOutput("plot")), 
-      #  tabPanel("Summary", verbatimTextOutput("summary")), 
-      #  tabPanel("Table", tableOutput("table"))
+    
+      #mainPanel( plotOutput("mainPlot"), textOutput("text1") ,textOutput("text2"), plotOutput("currencyPlot"))
+      mainPanel(
+        tabsetPanel(
+          tabPanel("Plot", plotOutput("mainPlot"), plotlyOutput("plotlyPlot"), verbatimTextOutput("event")), 
+          tabPanel("Currency", plotOutput("currencyPlot")), 
+          tabPanel("Table", dataTableOutput("table"))
+        )
       )
-    )
-#)
+   )
+)
+
 
 shinyApp(ui = ui, server = server)
