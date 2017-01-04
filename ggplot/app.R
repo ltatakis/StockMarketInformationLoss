fa76f06dcb8d5ba6301ed163de418d5bb9bce5c5
Lefteris Tatakis <teris.tat@gmail.com>
2017-01-04 22:53:42 +0200

    added multiple stocks in play- serious refactoring needed tomorrow

--- ggplot/app.R
+++ ggplot/app.R
@@ -4,21 +4,34 @@ library(shiny)
 
 server <- function(input, output) {
   # need to add this in a reactive expression and add multiple stocks
-  stockData <- getSymbols("GOOG", src = "google",  auto.assign = FALSE)
-  colnames(stockData) <- c("Open", "High", "Low", "Price", "Volume")
-  dataInput = data.frame( Date = index(stockData), stockData)
-  zoom <- reactiveValues(data = dataInput)
-   
+  outputSrc = "google"
+  
+  getSymbData <- reactive({
+    if(input$select == "GOOG") outputSrc = "google"
+    else source = "yahoo"
+    
+    stockData <- getSymbols(input$select, src = outputSrc,  auto.assign = FALSE)
+    colnames(stockData) <- c("Open", "High", "Low", "Price", "Volume")
+    data.frame( Date = index(stockData), stockData)
+    
+  })
+  zoom <- reactiveValues(data = NULL )
   #state array
+   
   arrayOfChange = as.data.frame(matrix( 0, ncol=2, nrow=1))
   names(arrayOfChange) = c( "State", "Data" )
   stateData <- reactiveValues( states = 0 )
   arrayData <- reactiveValues( stateArray = arrayOfChange )
+  #initialValueForNewPoints <-  getSymbData()
   
-  data <- reactiveValues(newPoints = dataInput)
+  #Initial values for main plot
+  data <- reactiveValues(
+    newPoints = NULL
+   )
   
   observeEvent(input$plot_brush, {
-    data$newPoints <-  brushedPoints(dataInput, input$plot_brush, "Date", "Price") 
+    mainData <- getSymbData()
+    data$newPoints <-  brushedPoints(mainData, input$plot_brush, "Date", "Price") 
     stateData$states <- stateData$states + 1
     arrayData$stateArray = rbind(arrayData$stateArray, c(stateData$states, nrow(data$newPoints) ))
   })
@@ -45,7 +58,10 @@ server <- function(input, output) {
       }
     })
     
-    ggplot(data = zoom$data , aes(x= Date, y = Price ) ) + 
+    if(is.null(zoom$data)) queryData = getSymbData()
+    else queryData = zoom$data
+    
+    ggplot(data = queryData , aes(x= Date, y = Price ) ) + 
       geom_line( colour = "#0072B2"  ) +
       theme_bw()
     
@@ -53,7 +69,9 @@ server <- function(input, output) {
   
   
   output$mainPlot <- renderPlot({
-    ggplot(data = data$newPoints, aes(x= Date, y = Price ) ) + 
+    if(is.null(data$newPoints)) data$newPoints =  getSymbData()
+    
+    ggplot(data = data$newPoints, aes(x=Date, y = Price ) ) + 
       geom_line( colour = "#0072B2"  ) +
       theme_bw() 
   })
@@ -78,39 +96,19 @@ server <- function(input, output) {
     viewFinancials(x.f)
   })
   
-  #lapply(fin, function(x) x$IS$A["Operating Income", ] / x$IS$A["Total Revenue",])
   
-  #co.f <- getFinancials("GOOG", "BS" , "A" ,auto.assign=FALSE)
-  #ratios = data.frame( Ratio = index( co.f),  co.f)
-  #viewFinancials(co.f) 
-  #print("-------------1")
-  #print(head(co.f))
-  #output$financial <- renderDataTable (viewFinancials(co.f))
   
-  observeEvent(input$y1, {  zoom$data <- subset(dataInput, Date > as.Date("2016-01-4") )
-                            #stateData$states <- stateData$states + 1
-                            #arrayData$stateArray <- rbind(arrayData$stateArray, c(stateData$states, nrow(data$newPoint) ))
+  observeEvent(input$y1, {  zoom$data <- subset(getSymbData(), Date > as.Date("2016-01-4") )
                           })
-  observeEvent(input$y5, {  zoom$data <- subset(dataInput, Date > as.Date("2012-01-4") )
-                            #stateData$states <- stateData$states + 1
-                            #arrayData$stateArray <- rbind(arrayData$stateArray, c(stateData$states, nrow(data$newPoint) ))
+  observeEvent(input$y5, {  zoom$data <- subset(getSymbData(), Date > as.Date("2012-01-4") )
                           })
-  observeEvent(input$y10,{ zoom$data <- subset(dataInput, Date > as.Date("2007-01-4") ) 
-                            #stateData$states <- stateData$states + 1
-                            #arrayData$stateArray <- rbind(arrayData$stateArray, c(stateData$states, nrow(data$newPoint) ))
+  observeEvent(input$y10,{ zoom$data <- subset(getSymbData(), Date > as.Date("2007-01-4") ) 
                           })
-  observeEvent(input$m3, {  zoom$data <- subset(dataInput, Date > as.Date("2016-09-1") ) 
-                            #stateData$states <- stateData$states + 1
-                            #arrayData$stateArray = rbind(arrayData$stateArray, c(stateData$states, nrow(data$newPoint) ))
+  observeEvent(input$m3, {  zoom$data <- subset(getSymbData(), Date > as.Date("2016-09-1") ) 
                           })
-  observeEvent(input$m6, { zoom$data <- subset(dataInput, Date > as.Date("2016-06-1") ) 
-                            #stateData$states <- stateData$states + 1
-                            #arrayData$stateArray = rbind(arrayData$stateArray, c(stateData$states, nrow(data$newPoint) ))
+  observeEvent(input$m6, { zoom$data <- subset(getSymbData(), Date > as.Date("2016-06-1") ) 
                           })
-  observeEvent(input$all,{  zoom$data <- dataInput 
-                            #stateData$states <- stateData$states + 1
-                            #arrayData$stateArray = rbind(arrayData$stateArray, c(stateData$states, nrow(data$newPoint)))
-                          })
+  observeEvent(input$all,{  zoom$data <- getSymbData() })
 }
 
 
@@ -128,8 +126,10 @@ ui <- fluidPage(
   mainPanel(
     # TODO add navbar ? for currency also?
     fluidRow( column(1, p("Stock selection:") ),
-              column(7, selectInput("select", label = NULL,  choices = list("GOOG" = 1, "AMZN" = 2, "FTSE" = 3), 
-                                                        selected = 1)
+              column(7, selectInput("select",
+                                    label = NULL,  
+                                    choices = list("SPY" = "SPY", "Google" = "GOOG","Amazon" = "AMZN", "Microsoft"="MSFT" ), 
+                                                        selected = "GOOG")
                         )
               ),
     
