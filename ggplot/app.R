1984324e74da73a71e16fa4ebd1f62d9897ac67b
proofreadinguk <mbax9et3@Lefteris-MacBook-Pro.local>
2017-01-04 19:04:53 +0200

    starting date selection by buttons

--- ggplot/app.R
+++ ggplot/app.R
@@ -1,5 +1,4 @@
 library(quantmod)
-
 library(ggplot2)
 library(shiny)
 
@@ -8,21 +7,21 @@ server <- function(input, output) {
   stockData <- getSymbols("GOOG", src = "google",  auto.assign = FALSE)
   colnames(stockData) <- c("Open", "High", "Low", "Price", "Volume")
   dataInput = data.frame( Date = index(stockData), stockData)
-  #print(head(dataInput))
+
+  dateButtons <- reactiveValues(data = '')
   
+  #state array
   arrayOfChange = as.data.frame(matrix(0, ncol=2, nrow=1))
   names(arrayOfChange) = c( "State", "Data" )
-  #print(arrayOfChange)
-  
   stateData <- reactiveValues(states = 0)
   arrayData <- reactiveValues( stateArray = arrayOfChange )
   
   data <- reactiveValues(newPoints = dataInput)
+  
   observeEvent(input$plot_brush, {
     data$newPoints <-  brushedPoints(dataInput, input$plot_brush, "Date", "Price") 
     stateData$states <- stateData$states + 1
     arrayData$stateArray = rbind(arrayData$stateArray, c(stateData$states, nrow(data$newPoints) ))
-    #print(arrayData$stateArray)
   })
 
   outputPlot <- ggplot(data = dataInput , aes(x= Date, y = Price ) ) + 
@@ -31,7 +30,29 @@ server <- function(input, output) {
  
   
   #start up plot
-  output$zoomPlot <- renderPlot({outputPlot})
+  output$zoomPlot <- renderPlot({
+    
+    dat <- data.frame(x = numeric(0), y = numeric(0))
+    withProgress(message = 'Making plot', value = 0, {
+      # Number of times we'll go through the loop
+      n <- 10
+      
+      for (i in 1:n) {
+        # Each time through the loop, add another row of data. This is
+        # a stand-in for a long-running computation.
+        dat <- rbind(dat, data.frame(x = rnorm(1), y = rnorm(1)))
+        
+        # Increment the progress bar, and update the detail text.
+        incProgress(1/n, detail = paste("Doing part", i))
+        
+        # Pause for 0.1 seconds to simulate a long computation.
+        Sys.sleep(0.1)
+      }
+    })
+    
+    outputPlot
+    
+  })
   
   
   output$mainPlot <- renderPlot({
@@ -45,12 +66,17 @@ server <- function(input, output) {
   output$stateChangePlot <- renderPlot({
     ggplot(data = arrayData$stateArray, aes(x= State, y = Data ) ) + 
       geom_line( colour = "#0072B2"  ) +
-      labs( title = "Amount of information shown in Main Plot due to interactions by the user") +
+      labs( title = "Amount of information shown in the main plot due to interactions by the user") +
       theme_bw() 
   })
   
   output$infoOfPoint <- renderPrint({
-    nearPoints(data$newPoints, input$main_plot_click , xvar = "Date", yvar =  "Price",threshold = 10, maxpoints = 2)
+    if (is.null(input$main_plot_click)) return("No selected data.")
+    else {
+      keeprows <- nearPoints(data$newPoints, input$main_plot_click , xvar = "Date", yvar ="Price", threshold = 10, maxpoints = 1)
+      print(subset(keeprows, select=c("Price", "Open", "High", "Low")))
+    }
+    
   })
   
   output$financialsText <- renderTable({
@@ -69,6 +95,13 @@ server <- function(input, output) {
   #print(head(co.f))
   #output$financial <- renderDataTable (viewFinancials(co.f)) 
   
+  observeEvent(input$y1, {  data$newPoints <- subset(dataInput, Date > as.Date("2016-01-4") ) })
+  observeEvent(input$y5, {  data$newPoints <- subset(dataInput, Date > as.Date("2012-01-4") ) })
+  observeEvent(input$y10,{  data$newPoints <- subset(dataInput, Date > as.Date("2007-01-4") ) })
+  observeEvent(input$d3, {  data$newPoints <- subset(dataInput, Date > as.Date("2016-12-4") ) }) 
+  observeEvent(input$m3, {  data$newPoints <- subset(dataInput, Date > as.Date("2016-06-4") ) })
+  observeEvent(input$m6, {  data$newPoints <- subset(dataInput, Date > as.Date("2016-09-4") ) })
+  observeEvent(input$all,{  data$newPoints <- dataInput })
 }
 
 
@@ -76,23 +109,35 @@ ui <- fluidPage(
   h1("Stockmarket View of Data" , align = "center"),
   hr(),
   sidebarLayout(
-    sidebarPanel( h3("Results of Interactions"), 
+    sidebarPanel( h2("Information of Selected point"),
+                  verbatimTextOutput("infoOfPoint"),  
+                  h3("Results of Interactions"), 
                   p("Amount of information in each state of the system"), 
                   plotOutput("stateChangePlot"),
-                  hr(),
-                  h2("Information of Selected point"),
-                  verbatimTextOutput("infoOfPoint"),  width = 4),
+                  width = 4),
+  
   mainPanel(
-    fluidRow(
-      column(12, plotOutput("mainPlot", hover = "main_plot_hover" , click = "main_plot_click")),
-      column(12,  plotOutput("zoomPlot", 
+    # need to allow these to set brushing distance
+    fluidRow( column(7, 
+                     actionButton("d3", "3D"),
+                     actionButton("w1", "1W"),
+                     actionButton("M3", "3M"), 
+                     actionButton("m6", "6M"),
+                     actionButton("y1", "1Y"),
+                     actionButton("y5", "5Y"),
+                     actionButton("y10", "10Y"),
+                     actionButton("all", "All")
+                )),
+    fluidRow( column(12, plotOutput("mainPlot", hover = "main_plot_hover" , click = "main_plot_click"))),
+    fluidRow( column(12, plotOutput("zoomPlot", 
                               hover = "plot_hover",
                               brush = brushOpts(id = "plot_brush", fill = "#ccc", direction = "x"),
                               height= "150"
-                             )
-                  )
+                          )
+            )
       )
-    ), position = "right"
+    ), 
+  position = "right"
   )
   #, dataTableOutput("table"))
   #sidebarPanel(tableOutput("financialsText"), width = 3)
